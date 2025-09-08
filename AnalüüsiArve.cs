69d6f596efe5f994d01161709d9e6c128a7577b2
opilane <opilane@HP-CZC2349HTQ>
2025-09-08 11:37:24 +0300

    bob3

---
+++ AnalüüsiArve.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_1
+{
+    internal class AnalüüsiArve
+    {
+        public static double[] teksti_arvud()
+        {
+            Console.WriteLine("Siseta arvud koma või tühikuga earaldatuld: ");
+            string sisend = Console.ReadLine();
+            char[] eraldajad = new char[] {' '}; // ';'
+
+            string[] osad = sisend.Split(eraldajad, StringSplitOptions.RemoveEmptyEntries);
+            double[] arvud = new double[osad.Length];
+            for(int i = 0; i < osad.Length; i++)
+            {
+                arvud[i] = Convert.ToDouble(osad[i]);
+            }
+            return arvud;
+        }
+        public static Tuple<double, double, double> AnalüüsiArved(double[] arvud)
+        {
+            double summa = arvud.Sum();
+            double keskmine = arvud.Average();
+            double korrutis = 1;
+
+            foreach (double arv in arvud)
+            {
+                korrutis *= (double)arv;
+            }
+            return Tuple.Create(summa, keskmine, korrutis);
+        }
+    }
+}
---
+++ ArvuTöötlus.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace c_1
+{
+    internal class ArvuTöötlus
+    {
+        Random rnd = new Random();
+
+        public int[] GenereeriRuudud(int min, int max)
+        {
+            // Genereerime 2 juhuslikku täisarvu vahemikus [-100, 100]
+            int N = rnd.Next(-100, 101);
+            int M = rnd.Next(-100, 101);
+
+            // Leiame väiksema ja suurema väärtuse, et teha vahemik
+            int start = Math.Min(N, M);
+            int end = Math.Max(N, M);
+
+            int length = end - start + 1; //Max - Min arv
+            int[] ruudud = new int[length]; //Loome uus masiiv ruudud
+
+            Console.WriteLine($"Min_Number = {start}, Max_number = {end}, Vahemik = {length} arvud");
+            Console.WriteLine("-----------");
+
+            // Täidame massiivi ruutudega
+            for (int i = 0; i < length; i++)
+            {
+                int number = start + i;
+                ruudud[i] = number * number;
+            }
+
+            // Prindime tulemused koos algväärtustega
+            for (int i = 0; i < length; i++)
+            {
+                int number = start + i;
+                Console.WriteLine($"{number} → {ruudud[i]}");
+            }
+
+            return ruudud;
+        }
+    }
+}
