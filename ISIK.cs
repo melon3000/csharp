50652ec5230aa16dfa767a91b74000877c639275
opilane <opilane@HP-CZC2349HTQ>
2025-09-08 09:37:53 +0300

    BOB2

---
+++ ISIK.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace c_1
+{
+    internal class Isik
+    {
+        public string eesnimi;
+        public string perenimi;
+        public int synniaasta;
+
+        public Isik() { }
+        public Isik(string eesnimi, string perenimi, int synniaasta)
+        {   
+            this.eesnimi = eesnimi;
+            this.perenimi = "tundmatu";
+            this.synniaasta = 2022;
+        }
+
+        public void Prindi()
+        {
+            Console.WriteLine($"{eesnimi}, {perenimi}, {synniaasta}");
+        }
+
+    }
+}
